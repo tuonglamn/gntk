fd4ef611f9a6a34caace75e8541312b1172a8fb9
tuonglamnguyen <tuonglamn@gmail.com>
2017-05-06 22:09:56 +0700

    new start

---
+++ src/main/java/com/lam/gntk/GntkApplication.java
@@ -0,0 +1,12 @@
+package com.lam.gntk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GntkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GntkApplication.class, args);
+	}
+}
---
+++ src/main/java/com/lam/gntk/web/models/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Lam Nguyen
+ *
+ */
+package com.lam.gntk.web.models;
