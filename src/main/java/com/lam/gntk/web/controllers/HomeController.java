5480ae8db40b2d88d635189d1a85ec1f652fde58
tuonglamnguyen <tuonglamn@gmail.com>
2017-05-06 22:20:08 +0700

    fixed new Home start page

---
+++ src/main/java/com/lam/gntk/web/controllers/HomeController.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.lam.gntk.web.controllers;
+
+/**
+ * @author Lam Nguyen
+ *
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+@RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
