c3bae65ac1305a7a2283129c0f29ab8452fca1dc
tuonglamnguyen <tuonglamn@gmail.com>
2017-05-07 22:59:32 +0700

    fixed Dropbox Drive

--- src/main/java/com/lam/gntk/web/controllers/HomeController.java
+++ src/main/java/com/lam/gntk/web/controllers/HomeController.java
@@ -11,10 +11,14 @@ package com.lam.gntk.web.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class HomeController {
-@RequestMapping("/")
+	
+	@RequestMapping("/")
     public String index() {
+		
         return "index";
+        
     }
 }
