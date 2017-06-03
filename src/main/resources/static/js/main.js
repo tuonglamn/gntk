31763bc2db4ee1d806cdc6af1d6dca3d680e3c7c
tuonglamnguyen <tuonglamn@gmail.com>
2017-06-03 16:32:10 +0700

    fixed

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,7 +1,25 @@
 requirejs.config({
-    paths: {
-        'jquery': 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min',
-        'mobile-menu': './mobile-menu',
-        'scroll-top': './scroll-top'
+    baseUrl: '/js/',
+	paths: {
+        'jquery': 'bower/jquery/jquery',
+        'angular': 'bower/angular/angular',
+        'googlemaps': 'bower/googlemaps-amd/googlemaps',
+        'async': 'bower/requirejs-plugins/async',
+        'map': 'lib/map',
+        'app': 'lib/app'
+    },
+    googlemaps: {
+    	params: {
+    		key: 'AIzaSyAzIDW9AZseqnR8vcY12OtfQPIV_MODSXQ'
+    	}
+    },
+    shim: {
+    	'jquery': [],
+    	'app': ['jquery']
     }
+});
+
+require(['jquery','app','map'],function($, app, map){
+	app.init();
+	map.mapStart();
 });
